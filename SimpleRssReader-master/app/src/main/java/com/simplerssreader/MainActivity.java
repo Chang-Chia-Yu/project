7c94770915d9050849fbeb27a337037ebec9e0d8
Chang Chia-Yu <lovelin0717wanyuyo@gmail.com>
2017-07-03 21:52:54 +0800

    加註解

--- SimpleRssReader-master/app/src/main/java/com/simplerssreader/MainActivity.java
+++ SimpleRssReader-master/app/src/main/java/com/simplerssreader/MainActivity.java
@@ -3,13 +3,13 @@ package com.simplerssreader;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;//
 
 public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main);//
         if (savedInstanceState == null) {
             addRssFragment();
         }
