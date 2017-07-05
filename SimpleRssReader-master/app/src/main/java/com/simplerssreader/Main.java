6a86b04ca10ceb8d1f9a122f93a07bb7a8c3bc01
WaterBall <johnny850807@gmail.com>
2017-07-05 15:56:52 +0800

    成功rss連結

--- SimpleRssReader-master/app/src/main/java/com/simplerssreader/Main.java
+++ SimpleRssReader-master/app/src/main/java/com/simplerssreader/Main.java
@@ -5,6 +5,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
+import static com.simplerssreader.RssFragment.LINK_RES;
+
 
 public class Main extends AppCompatActivity {
     @Override
@@ -17,9 +19,12 @@ public class Main extends AppCompatActivity {
     }
 
     private void addRssFragment() {
+        String link = getIntent().getStringExtra(LINK_RES);
+
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
-        RssFragment fragment = new RssFragment();
+
+        RssFragment fragment = RssFragment.getInstance(link);
         transaction.add(R.id.fragment_container, fragment);
         transaction.commit();
     }
